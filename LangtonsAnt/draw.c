465e2476ad9824b7c032ccdc2707a15db6f3962c
AndrisBorbas <andris.borbas@gmail.com>
2018-10-19 13:41:26 +0200

    Done pausable simulation

--- LangtonsAnt/draw.c
+++ LangtonsAnt/draw.c
@@ -1,4 +1,5 @@
 #include <SDL.h>
+#include <SDL_image.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
@@ -17,12 +18,20 @@ void memset32(void * dest, Uint32 value, uintptr_t size)
 	}
 }
 
+Uint32 ftick(Uint32 ms, void *param)
+{
+	SDL_Event ev;
+	ev.type = SDL_USEREVENT;
+	SDL_PushEvent(&ev);
+	return ms;
+}
+
 //Width of the window
 volatile int SCREEN_WIDTH = 960;
 //Height of the window
 volatile int SCREEN_HEIGHT = 960;
 //Number of pixels per grid square
-volatile int SCALE = 20;
+volatile int SCALE = 4;
 //Number of pixels between grid squares
 volatile int SPACING = 1;
 //Number of pixels the ant is smaller than the grid (has to be at least 1)
@@ -46,104 +55,108 @@ enum HEXARGB
 	BLACK = 0xFF000000
 };
 
+//Stores the properties of the ant
 typedef struct Ant
 {
 	int x, y, heading, lasttile;
 }Ant;
 
-//Starts up SDL and creates window
+//Frees media and shuts down SDL
 void close(Uint32**, SDL_Window**, SDL_Renderer**, SDL_Texture**);
 
-//Frees media and shuts down SDL
-bool init(Uint32**, Uint32, SDL_Window**, SDL_Renderer**, SDL_Texture**);
+//Starts up SDL and creates window
+bool initSDL(SDL_Window**, SDL_Renderer**, SDL_Texture**);
 
-//meow
-bool antgorithm(Uint32**, Ant*);
+//Initializes the pixel arrays
+bool initPixels(Uint32**, Uint32***);
+
+//Converts pixel texture to sdl pixel format
+bool convertPixels(Uint32**, Uint32***);
+
+//The alogorithm for the ants movement
+bool antgorithm(Uint32***, Ant*);
 
 //move ant and invert past location
-bool moveAnt(Uint32**, Ant*);
+bool moveAnt(Uint32***, Ant*);
 
-bool moveAnt(Uint32** pixels, Ant* ant)
+bool moveAnt(Uint32*** pixelTex, Ant* ant)
 {
-	//temporary speed booster
-	for (int db = 0; db < 10; db++) {
-		if(!antgorithm(pixels, ant))return false;
-		lepes++;
-		printf("%d. lepes: ", lepes);
+	if (!antgorithm(pixelTex, ant))return false;
+	lepes++;
+	printf("%d. lepes: ", lepes);
 
-		//printf("%d  %d\n", ant->y, ant->y - SCALE / 2 + SPACING);
-		//(*pixels)[(ant->x - SCALE - SPACING + ANTMARGIN) + (ant->y - SCALE - SPACING + ANTMARGIN) * SCREEN_WIDTH] = 0xFF00FF00;
+	//printf("%d  %d\n", ant->y, ant->y - SCALE / 2 + SPACING);
+	//(*pixels)[(ant->x - SCALE - SPACING + ANTMARGIN) + (ant->y - SCALE - SPACING + ANTMARGIN) * SCREEN_WIDTH] = 0xFF00FF00;
 
-		//invert last location
-		for (int i = (ant->x - SCALE); i < (ant->x + SCALE - SPACING); i++)
+	//invert last location
+	for (int i = (ant->x - SCALE); i < (ant->x + SCALE - SPACING); i++)
+	{
+		for (int j = (ant->y - SCALE); j < (ant->y + SCALE - SPACING); j++)
 		{
-			for (int j = (ant->y - SCALE); j < (ant->y + SCALE - SPACING); j++)
+			if (i<0 || i>SCREEN_WIDTH || j<0 || j>SCREEN_HEIGHT)
 			{
-				if (i<0 || i>SCREEN_WIDTH || j<0 || j>SCREEN_HEIGHT)
-				{
-					printf("ERROR: ant out of bounds at x=%d, y=%d.", i, j);
-					return false;
-				}
-				switch (ant->lasttile)
-				{
-				case GRAY:
-					(*pixels)[i + j * SCREEN_WIDTH] = DARKWHITE;
-					break;
-				case DARKWHITE:
-					(*pixels)[i + j * SCREEN_WIDTH] = GRAY;
-					break;
-				default:
-					break;
-				}
+				printf("ERROR: ant out of bounds at x=%d, y=%d.", i, j);
+				return false;
+			}
+			switch (ant->lasttile)
+			{
+			case GRAY:
+				(*pixelTex)[i][j] = DARKWHITE;
+				break;
+			case DARKWHITE:
+				(*pixelTex)[i][j] = GRAY;
+				break;
+			default:
+				break;
 			}
 		}
+	}
 
 
 
-		//printf("%d ", ant->heading);
-		switch (ant->heading)
+	//printf("%d ", ant->heading);
+	switch (ant->heading)
+	{
+	case UP:
+		ant->y = ant->y - SCALE * 2;
+		break;
+	case DOWN:
+		ant->y = ant->y + SCALE * 2;
+		break;
+	case RIGHT:
+		ant->x = ant->x + SCALE * 2;
+		break;
+	case LEFT:
+		ant->x = ant->x - SCALE * 2;
+		break;
+	default:
+		break;
+	}
+	//printf("%d\n", ant->heading);
+	for (int i = (ant->x - SCALE + ANTMARGIN); i < (ant->x + SCALE - SPACING - ANTMARGIN); i++)
+	{
+		for (int j = (ant->y - SCALE + ANTMARGIN); j < (ant->y + SCALE - SPACING - ANTMARGIN); j++)
 		{
-		case UP:
-			ant->y = ant->y - SCALE * 2;
-			break;
-		case DOWN:
-			ant->y = ant->y + SCALE * 2;
-			break;
-		case RIGHT:
-			ant->x = ant->x + SCALE * 2;
-			break;
-		case LEFT:
-			ant->x = ant->x - SCALE * 2;
-			break;
-		default:
-			break;
-		}
-		//printf("%d\n", ant->heading);
-		for (int i = (ant->x - SCALE + ANTMARGIN); i < (ant->x + SCALE - SPACING - ANTMARGIN); i++)
-		{
-			for (int j = (ant->y - SCALE + ANTMARGIN); j < (ant->y + SCALE - SPACING - ANTMARGIN); j++)
+			if (i<0 || i>SCREEN_WIDTH || j<0 || j>SCREEN_HEIGHT)
 			{
-				if (i<0 || i>SCREEN_WIDTH || j<0 || j>SCREEN_HEIGHT)
-				{
-					printf("ERROR: ant out of bounds at x=%d, y=%d.", i, j);
-					return false;
-				}
-				(*pixels)[i + j * SCREEN_WIDTH] = 0xFFFF0000;
+				printf("ERROR: ant out of bounds at x=%d, y=%d.", i, j);
+				return false;
 			}
+			(*pixelTex)[i][j] = 0xFFFF0000;
 		}
 	}
 	return true;
 }
 
-bool antgorithm(Uint32** pixels, Ant* ant) {
+bool antgorithm(Uint32*** pixelTex, Ant* ant) {
 	//printf("%u\n%u\n%u\n\n", ((*pixels)[(ant->x - SCALE - SPACING + ANTMARGIN) + ((ant->y - SCALE - SPACING + ANTMARGIN)*SCREEN_WIDTH)]), BLACK, 0xFF000000);
 	int xpos = (ant->x - SCALE - SPACING + ANTMARGIN);
 	int ypos = (ant->y - SCALE - SPACING + ANTMARGIN);
 
 	if (xpos < 0 || xpos > SCREEN_WIDTH || ypos < 0 || ypos > SCREEN_HEIGHT)return false;
 
-	int position = xpos + (ypos * SCREEN_WIDTH);
-	if (((*pixels)[position]) == BLACK || ((*pixels)[position]) == GRAY)
+	//int position = xpos + (ypos * SCREEN_WIDTH);
+	if (((*pixelTex)[xpos][ypos]) == BLACK || ((*pixelTex)[xpos][ypos]) == GRAY)
 	{
 		ant->heading = ant->heading + 90;
 		if (ant->heading > 270) ant->heading = ant->heading - 360;
@@ -151,7 +164,7 @@ bool antgorithm(Uint32** pixels, Ant* ant) {
 		printf("jobb\n");
 		return true;
 	}
-	if (((*pixels)[position]) == DARKWHITE)
+	if (((*pixelTex)[xpos][ypos]) == DARKWHITE)
 	{
 		ant->heading = ant->heading - 90;
 		if (ant->heading < 0) ant->heading = ant->heading + 360;
@@ -162,7 +175,7 @@ bool antgorithm(Uint32** pixels, Ant* ant) {
 	return false;
 }
 
-bool init(Uint32** pixels, Uint32*** pixelarr, SDL_Window** gWindow, SDL_Renderer** gRenderer, SDL_Texture** gTexture)
+bool initSDL(SDL_Window** gWindow, SDL_Renderer** gRenderer, SDL_Texture** gTexture)
 {
 	//Initialization flag
 	bool success = true;
@@ -192,33 +205,48 @@ bool init(Uint32** pixels, Uint32*** pixelarr, SDL_Window** gWindow, SDL_Rendere
 
 			//Initialize screen texture
 			*gTexture = SDL_CreateTexture(*gRenderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-			//Initialize pixel array
-			*pixels = malloc(SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(**pixels));
-
-			//Initialize 2D pixel texture array
-			*pixelarr = (Uint32 ***)malloc(sizeof(Uint32 *) * SCREEN_WIDTH);
-			*pixelarr[0] = (Uint32 **)malloc(sizeof(Uint32) * SCREEN_HEIGHT * SCREEN_WIDTH);
-			for (int i = 0; i < SCREEN_WIDTH; i++)
-				(*pixelarr)[i] = (**pixelarr + SCREEN_HEIGHT * i);
-
-			//Override all pixels
-			memset32(*pixels, BLACK, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
-			memset32(**pixelarr, BLACK, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
 		}
 	}
 
 	return success;
 }
 
-void close(Uint32** pixels, SDL_Window** gWindow, SDL_Renderer** gRenderer, SDL_Texture** gTexture)
+bool initPixels(Uint32** pixels, Uint32*** pixelarr) {
+	//Initialize pixel array
+	*pixels = malloc(SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(**pixels));
+
+	//Initialize 2D pixel texture array
+	*pixelarr = (Uint32 ***)malloc(sizeof(Uint32 *) * SCREEN_WIDTH);
+	*pixelarr[0] = (Uint32 **)malloc(sizeof(Uint32) * SCREEN_HEIGHT * SCREEN_WIDTH);
+	for (int i = 0; i < SCREEN_WIDTH; i++)
+		(*pixelarr)[i] = (**pixelarr + SCREEN_HEIGHT * i);
+
+	//Override all pixels
+	memset32(*pixels, BLACK, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+	memset32(**pixelarr, BLACK, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+	return true;
+}
+
+bool convertPixels(Uint32** pixels, Uint32*** pixelarr) {
+	for (int i = 0; i < SCREEN_WIDTH; i++)
+	{
+		for (int j = 0; j < SCREEN_HEIGHT; j++)
+		{
+			(*pixels)[i + j * SCREEN_WIDTH] = (*pixelarr)[i][j];
+		}
+	}
+}
+
+void close(Uint32** pixels, SDL_Window** gWindow, SDL_Renderer** gRenderer, SDL_Texture** tPixelTexture, SDL_Texture** tMainMenu)
 {
 	//Free up allocated pixel array
 	free(*pixels);
 
 	//Destroy texture
-	SDL_DestroyTexture(*gTexture);
-	*gTexture = NULL;
+	SDL_DestroyTexture(*tPixelTexture);
+	SDL_DestroyTexture(*tMainMenu);
+	*tMainMenu = NULL;
+	*tPixelTexture = NULL;
 
 	//Destroy renderer
 	SDL_DestroyRenderer(*gRenderer);
@@ -257,113 +285,155 @@ void gmain(int argc, char ** argv)
 	//The pixel renderer
 	SDL_Renderer *gRenderer = NULL;
 
-	SDL_Texture *gTexture = NULL;
+	SDL_Texture *tPixelTexture = NULL;
+	SDL_Texture *tMainMenu = NULL;
 	Uint32 *pixels = NULL;
-	Uint32 **pixelarr = NULL;
+	Uint32 **pixelTex = NULL;
 
 	//Start up SDL and create window
-	if (!init(&pixels, &pixelarr, &gWindow, &gRenderer, &gTexture))
+	if (!initSDL(&gWindow, &gRenderer, &tPixelTexture))
 	{
-		printf("Failed to initialize!\n");
+		printf("Failed to initialize: %s", SDL_GetError());
+		exit(1);
 	}
-	else
+	//Load main menu background
+	tMainMenu = IMG_LoadTexture(gRenderer, "MainMenu.png");
+	if (tMainMenu == NULL)
 	{
-		SDL_Event event;
+		SDL_Log("Nem nyithato meg a kepfajl: %s", IMG_GetError());
+		exit(1);
+	}
+	//Render main menu
+	SDL_RenderClear(gRenderer);
+	SDL_RenderCopy(gRenderer, tMainMenu, NULL, NULL);
+	SDL_RenderPresent(gRenderer);
 
-		while (!quit)
+
+
+	//Initialize pixel textures
+	initPixels(&pixels, &pixelTex);
+
+	//Create base tickrate
+	SDL_TimerID tick = SDL_AddTimer(16, ftick, NULL);
+
+	SDL_Event event;
+
+	while (!quit)
+	{
+		SDL_WaitEvent(&event);
+
+		switch (event.type)
 		{
-
-
-			SDL_WaitEvent(&event);
-
-			switch (event.type)
+		case SDL_QUIT:
+			quit = true;
+			break;
+		case SDL_USEREVENT:
+			while (Running)
 			{
-			case SDL_QUIT:
-				quit = true;
-				break;
-			case SDL_KEYDOWN:
-				switch (event.key.keysym.sym)
+				if (!moveAnt(&pixelTex, &ant))
 				{
-				case SDLK_SPACE:
-					if (!moveAnt(&pixels, &ant))
-					{
-        				ERROR = true;
-						quit = true;
-					}
-					else SDL_UpdateTexture(gTexture, NULL, pixels, SCREEN_WIDTH * sizeof(Uint32));
-					//if (Running)Running = false;
-					//else Running = true;
-					break;
-				case SDLK_w:
-					ant.heading = UP;
-					moveAnt(&pixels, &ant);
-					SDL_UpdateTexture(gTexture, NULL, pixels, SCREEN_WIDTH * sizeof(Uint32));
-					break;
-				case SDLK_s:
-					ant.heading = DOWN;
-					moveAnt(&pixels, &ant);
-					SDL_UpdateTexture(gTexture, NULL, pixels, SCREEN_WIDTH * sizeof(Uint32));
-					break;
-				case SDLK_d:
-					ant.heading = RIGHT;
-					moveAnt(&pixels, &ant);
-					SDL_UpdateTexture(gTexture, NULL, pixels, SCREEN_WIDTH * sizeof(Uint32));
-					break;
-				case SDLK_a:
-					ant.heading = LEFT;
-					moveAnt(&pixels, &ant);
-					SDL_UpdateTexture(gTexture, NULL, pixels, SCREEN_WIDTH * sizeof(Uint32));
-					break;
-					/*case SDL_MOUSEBUTTONUP:
-						if (event.button.button == SDL_BUTTON_LEFT)
-							leftMouseButtonDown = false;
-						break;
-					case SDL_MOUSEBUTTONDOWN:
-						if (event.button.button == SDL_BUTTON_LEFT)
-							leftMouseButtonDown = true;
-					case SDL_MOUSEMOTION:
-						if (leftMouseButtonDown)
-						{
-							//Get mouse position
-							int mouseX = event.motion.x;
-							int mouseY = event.motion.y;
-							printf("X:%d Y:%d    ", mouseX, mouseY);
-
-							//Set 2D texture
-							pixelarr[mouseX][mouseY] = 0xFFFF0000;
-							pixelarr[mouseX][mouseY + 1] = 0xFFFF0000;
-							pixelarr[mouseX + 1][mouseY] = 0xFFFF0000;
-							pixelarr[mouseX + 1][mouseY + 1] = 0xFFFF0000;
-
-							//printf("%d  ", pixelarr[mouseX][mouseY]);
-
-							//Convert 2D texture to 1D pixel array
-							for (int x = 0; x < SCREEN_WIDTH; x++)
-							{
-								for (int y = 0; y < SCREEN_HEIGHT; y++)
-								{
-									pixels[x + y * SCREEN_WIDTH] = pixelarr[x][y];
-								}
-							}
-							//pixels[mouseY * SCREEN_WIDTH + mouseX] = 0xFF00FF00;
-							//printf("%d |", pixels[mouseY * SCREEN_WIDTH + mouseX]);
-						}
-						break;*/
+					ERROR = true;
+					quit = true;
 				}
-				while (Running)
-				{
-					moveAnt(&pixels, &ant);
-					SDL_UpdateTexture(gTexture, NULL, pixels, SCREEN_WIDTH * sizeof(Uint32));
-				}
+				convertPixels(&pixels, &pixelTex);
+				SDL_UpdateTexture(tPixelTexture, NULL, pixels, SCREEN_WIDTH * sizeof(Uint32));
 				if (!ERROR) {
 					SDL_RenderClear(gRenderer);
-					SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+					SDL_RenderCopy(gRenderer, tPixelTexture, NULL, NULL);
 					SDL_RenderPresent(gRenderer);
 				}
+				SDL_WaitEvent(&event);
+				switch (event.type)
+				{
+				case SDL_QUIT:
+					quit = true;
+					break;
+				case SDL_KEYDOWN:
+					switch (event.key.keysym.sym)
+					{
+					case SDLK_SPACE:
+						//if (Running)Running = false;
+						/*else */Running = false;
+						break;
+					}
+				}
+			}
+			break;
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.sym)
+			{
+			case SDLK_g:
+				if (!moveAnt(&pixelTex, &ant))
+				{
+					ERROR = true;
+					quit = true;
+				}
+				convertPixels(&pixels, &pixelTex);
+				SDL_UpdateTexture(tPixelTexture, NULL, pixels, SCREEN_WIDTH * sizeof(Uint32));
+				//if (Running)Running = false;
+				//else Running = true;
+				break;
+			case SDLK_SPACE:
+				Running = true;
+				break;
+			case SDLK_w:
+				ant.heading = UP;
+				moveAnt(&pixels, &ant);
+				SDL_UpdateTexture(tPixelTexture, NULL, pixels, SCREEN_WIDTH * sizeof(Uint32));
+				break;
+			case SDLK_s:
+				ant.heading = DOWN;
+				moveAnt(&pixels, &ant);
+				SDL_UpdateTexture(tPixelTexture, NULL, pixels, SCREEN_WIDTH * sizeof(Uint32));
+				break;
+			case SDLK_d:
+				ant.heading = RIGHT;
+				moveAnt(&pixels, &ant);
+				SDL_UpdateTexture(tPixelTexture, NULL, pixels, SCREEN_WIDTH * sizeof(Uint32));
+				break;
+			case SDLK_a:
+				ant.heading = LEFT;
+				moveAnt(&pixels, &ant);
+				SDL_UpdateTexture(tPixelTexture, NULL, pixels, SCREEN_WIDTH * sizeof(Uint32));
+				break;
+				/*case SDL_MOUSEBUTTONUP:
+					if (event.button.button == SDL_BUTTON_LEFT)
+						leftMouseButtonDown = false;
+					break;
+				case SDL_MOUSEBUTTONDOWN:
+					if (event.button.button == SDL_BUTTON_LEFT)
+						leftMouseButtonDown = true;
+				case SDL_MOUSEMOTION:
+					if (leftMouseButtonDown)
+					{
+						//Get mouse position
+						int mouseX = event.motion.x;
+						int mouseY = event.motion.y;
+						printf("X:%d Y:%d    ", mouseX, mouseY);
+
+						//Set 2D texture
+						pixelTex[mouseX][mouseY] = 0xFFFF0000;
+						pixelTex[mouseX][mouseY + 1] = 0xFFFF0000;
+						pixelTex[mouseX + 1][mouseY] = 0xFFFF0000;
+						pixelTex[mouseX + 1][mouseY + 1] = 0xFFFF0000;
+
+						//printf("%d  ", pixelTex[mouseX][mouseY]);
+
+						//Convert 2D texture to 1D pixel array
+						for (int x = 0; x < SCREEN_WIDTH; x++)
+						{
+							for (int y = 0; y < SCREEN_HEIGHT; y++)
+							{
+								pixels[x + y * SCREEN_WIDTH] = pixelTex[x][y];
+							}
+						}
+						//pixels[mouseY * SCREEN_WIDTH + mouseX] = 0xFF00FF00;
+						//printf("%d |", pixels[mouseY * SCREEN_WIDTH + mouseX]);
+					}
+					break;*/
 			}
 		}
-		if (ERROR)
-		{
+		if (ERROR) {
 			while (!equit)
 			{
 				SDL_WaitEvent(&event);
@@ -373,7 +443,13 @@ void gmain(int argc, char ** argv)
 				}
 			}
 		}
-		//Free resources and close SDL
-		close(&pixels, &gWindow, &gRenderer, &gTexture);
+		else
+		{
+			SDL_RenderClear(gRenderer);
+			SDL_RenderCopy(gRenderer, tPixelTexture, NULL, NULL);
+			SDL_RenderPresent(gRenderer);
+		}
 	}
+	//Free resources and close SDL
+	close(&pixels, &gWindow, &gRenderer, &tPixelTexture, &tMainMenu);
 }
